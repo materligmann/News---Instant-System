c42130aeae6716f449b26192ca3f5852026a6cf6
Mathias <merligmann@gmail.com>
2023-12-19 17:59:48 +0100

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Mathias Erligmann on 19/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
